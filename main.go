14971cc711da80a7fac017fab224efee93545151
Alexey Samolyuk <aleshasam@mail.ru>
2024-12-04 22:15:12 +0700

    Получение города

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"demo/weather/geo"
+	"flag"
+	"fmt"
+)
 
+func main() {
+	city := flag.String("city", "", "Город пользователя")
+	//format := flag.Int("format", 1, "Формат вывода")
+	flag.Parse()
+
+	fmt.Println(*city)
+	geoData, err := geo.GetCurrentLocation(*city)
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println(geoData)
 }
